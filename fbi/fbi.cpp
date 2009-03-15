019ed366419363595d3c037f3acff2acef45526c
Andreas Stuehrk <andy@hammerhartes.de>
2009-03-15 20:39:36 +0100

    initial import of fbi, a small brainfck interpreter

---
+++ fbi/fbi.cpp
@@ -0,0 +1,150 @@
+//
+// A small brainfuck interpreter.
+//
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+
+class CellIterator
+{
+public:
+    CellIterator() : index_(0) {};
+   
+    char& operator*();
+    CellIterator& operator++();
+    CellIterator& operator--();
+protected:
+    int index_;
+    std::map<int, char> cells_;
+};
+
+char& CellIterator::operator*()
+{
+    return cells_[index_];
+}
+
+CellIterator& CellIterator::operator++()
+{
+    ++index_;
+    return *this;
+}
+
+CellIterator& CellIterator::operator--()
+{
+    --index_;
+    return *this;
+}
+
+class Interpreter
+{
+public:
+    bool run(const std::string&);
+protected:
+    CellIterator cell_;
+};
+
+
+bool Interpreter::run(const std::string& code)
+{
+    // First generate jump tables
+    std::vector<std::string::const_iterator> from;
+    std::map<std::string::const_iterator,
+            std::string::const_iterator> jump_from, jump_to;
+    for (std::string::const_iterator it=code.begin(); it != code.end(); ++it)
+    {
+        switch (*it)
+        {
+            case '[':
+                from.push_back(it);
+                break;
+
+            case ']':
+                if (!from.size())
+                    return false;
+                jump_from[from.back()] = it;
+                jump_to[it] = from.back();
+                from.pop_back();
+        }
+    }
+    for (std::string::const_iterator it=code.begin(); it != code.end(); ++it)
+        switch (*it)
+        {
+            case '<':
+                ++cell_;
+                continue;;
+
+            case '>':
+                --cell_;
+                continue;
+
+            case '+':
+                ++*cell_;
+                continue;
+            
+            case '-':
+                --*cell_;
+                continue;
+
+            case '.':
+                std::cout << *cell_;
+                std::cout.flush();
+                continue;
+
+            case ',':
+                std::cin.get(*cell_);
+                continue;
+
+            case '[':
+                if (!*cell_)
+                    it = jump_from[it];
+                continue;
+
+            case ']':
+                if (*cell_)
+                    it = jump_to[it];
+                continue;
+
+            case '\n':
+                continue;
+
+            default:
+                return false;
+        }
+    return true;
+}
+
+
+int main(int argc, char *argv[])
+{
+    std::string code;
+    switch (argc)
+    {
+        case 1:
+        {
+            // Read code from stdin
+            std::string line;
+            while (std::cin)
+            {
+                std::getline(std::cin, line);
+                code.append(line);
+            }
+            break;
+        }
+
+        case 2:
+            code.assign(argv[1]);
+            break;
+
+        default:
+            std::cerr << "Usage: fbi [<code>]" << std::endl;
+            return 1;
+    }
+
+    Interpreter interpreter;
+    if (!interpreter.run(code))
+        return 1;
+    return 0;
+}
