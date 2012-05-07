f3daffc889b71109bbf1cd1634cee1debe9e0e48
Sebastian Ramacher <s.ramacher@gmx.at>
2012-05-07 02:01:56 +0200

    fiforc: Fix FIFO issues.

--- fifoirc/fifoirc.py
+++ fifoirc/fifoirc.py
@@ -12,10 +12,9 @@ A simple IRC bot that pipes a FIFO into an IRC chan.
 from __future__ import with_statement
 
 from optparse import OptionParser
-from twisted.internet import protocol
+from twisted.internet.protocol import ReconnectingClientFactory
 from twisted.words.protocols import irc
-from twisted.internet import reactor, ssl, threads
-from twisted.protocols.basic import FileSender, LineReceiver
+from twisted.internet import error, reactor, ssl, threads
 import os, os.path
 import time
 import sys
@@ -28,7 +27,7 @@ class FifoIRC(irc.IRCClient):
 
   def __init__(self, *args, **kwargs):
     self.defered = None
-    self.open_fifo = None
+    self.fd = None
 
   def sendLine(self, line):
     if isinstance(line, unicode):
@@ -37,28 +36,32 @@ class FifoIRC(irc.IRCClient):
 
   def signedOn(self):
     self.join(self.factory.channel)
+    self.fd = os.open(self.factory.fifo, os.O_RDONLY | os.O_NONBLOCK)
     self.defered = threads.deferToThread(self.readFIFO)
 
   def connectionLost(self, reason):
-    if self.defered is not None and self.open_fifo is not None:
-      self.open_fifo.close()
+    if self.defered is not None and self.fd is not None:
+      os.close(self.fd)
+      self.fd = None
 
   def readFIFO(self):
     # This is not really in the spirit of twisted. But I am not motiviated
     # enough to implement in a sane way.
     try:
-      with open(self.factory.fifo, 'r') as self.open_fifo:
-        while True:
-          line = self.open_fifo.readline()
-          if not line:
-            time.sleep(1)
-            continue
+      while True:
+        d = os.read(self.fd, 1024)
+        if not d:
+          time.sleep(1)
+          continue
+
+        lines = d.split('\n')
+        for line in lines:
           reactor.callFromThread(self.msg, self.factory.channel,
                                  line.rstrip('\n\r'))
     except OSError:
       pass
 
-class FifoIRCFactory(protocol.ReconnectingClientFactory):
+class FifoIRCFactory(ReconnectingClientFactory):
   protocol = FifoIRC
 
   def __init__(self, channel, nickname, fifo):
@@ -69,11 +72,11 @@ class FifoIRCFactory(protocol.ReconnectingClientFactory):
   def buildProtocol(self, *args):
     print 'I: Connected.'
     self.resetDelay()
-    return protocol.ReconnectingClientFactory.buildProtocol(self, *args)
+    return ReconnectingClientFactory.buildProtocol(self, *args)
 
   def clientConnectionLost(self, connector, reason):
     print 'I: Connection lost: %s' % (str(reason.getErrorMessage()), )
-    return protocol.ReconnectingClientFactory.clientConnectionLost(self, connector,
+    return ReconnectingClientFactory.clientConnectionLost(self, connector,
                                                           reason)
 
   def clientConnectionFailed(self, connector, reason):
@@ -113,6 +116,7 @@ def main(args=None):
     reactor.connectSSL(args[0], opts.port, factory, ssl.ClientContextFactory())
   else:
     reactor.connectTCP(args[0], opts.port, factory)
+
   try:
     reactor.run()
   except KeyboardInterrupt:
